7663134da68d36322e31b0ccba35e914434588fe
MalteRei <MalteRei@users.noreply.github.com>
2019-12-22 07:27:19 +0100

    Rmi server in rcp application starts but is not accessible
    java.lang.ClassNotFoundException: edu.kit.palladio.rcp.IHelloWorldFromEclipse

--- edu.kit.palladio.rcp/src/edu/kit/palladio/rcp/HelloWorldFromEclipse.java
+++ edu.kit.palladio.rcp/src/edu/kit/palladio/rcp/HelloWorldFromEclipse.java
@@ -1,14 +1,21 @@
 package edu.kit.palladio.rcp;
 
+import java.io.Serializable;
 import java.rmi.RemoteException;
 import java.util.Date;
 
-public class HelloWorldFromEclipse implements IHelloWorldFromEclipse {
+public class HelloWorldFromEclipse implements IHelloWorldFromEclipse, Serializable {
 
+	
+	private static final long serialVersionUID = 1L;
+	
+	public HelloWorldFromEclipse() {
+		System.out.println("constructor of hello world from eclipse");
+	}
 	@Override
 	public String hello(String whatToSay) throws RemoteException {
 		Date timeArrived = new Date();
-		System.out.println("Hello from eclipse with message: "+whatToSay);
+		//System.out.println("Hello from eclipse with message: "+whatToSay);
 		return "eclipse said "+ whatToSay+ " at " + timeArrived.toString();
 	}
 
