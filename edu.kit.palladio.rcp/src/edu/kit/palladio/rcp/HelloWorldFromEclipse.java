f0308e405a1eee378ea6dd8b29952c70eb0c6a52
MalteRei <MalteRei@users.noreply.github.com>
2020-01-03 17:47:47 +0100

    Update HelloWorldFromEclipse.java
    Now it does not print anymore when calling constructor.

--- edu.kit.palladio.rcp/src/edu/kit/palladio/rcp/HelloWorldFromEclipse.java
+++ edu.kit.palladio.rcp/src/edu/kit/palladio/rcp/HelloWorldFromEclipse.java
@@ -9,13 +9,11 @@ public class HelloWorldFromEclipse implements IHelloWorldFromEclipse, Serializab
 	
 	private static final long serialVersionUID = 1L;
 	
-	public HelloWorldFromEclipse() {
-		System.out.println("constructor of hello world from eclipse");
-	}
+	
 	@Override
 	public String hello(String whatToSay) throws RemoteException {
 		Date timeArrived = new Date();
-		//System.out.println("Hello from eclipse with message: "+whatToSay);
+		System.out.println("Hello from eclipse with message: "+whatToSay);
 		return "eclipse said "+ whatToSay+ " at " + timeArrived.toString();
 	}
 
