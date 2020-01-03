b84defcd21301158977765436a8a7aa80e7a0146
MalteRei <MalteRei@users.noreply.github.com>
2020-01-03 09:25:17 +0100

    Update HelloWorldApp to be like client of smartgrid project
    https://github.com/kit-sdq/Smart-Grid-ICT-Resilience-Framework/blob/master/tests/smartgrid.newsimcontrol.tests/src/smartgrid/newsimcontrol/tests/client/TestClientRMI.java

--- rmiclient/HelloWorldApp.java
+++ rmiclient/HelloWorldApp.java
@@ -2,14 +2,35 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class HelloWorldApp{
+    private static final String hostName = "localhost";
+	private static final String lookupName = "IHelloWorldFromEclipse";
 
     public static void main(String args[]){
+
+        System.setProperty("java.security.policy", "file:client.policy");
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new SecurityManager());
+        }
+        try {
+            System.setProperty("java.rmi.server.hostname", "localhost");		
+            var registry = LocateRegistry.getRegistry(hostName);
+            IHelloWorldFromEclipse helloWorld = (IHelloWorldFromEclipse) registry.lookup(lookupName);
+            String result = helloWorld.hello("Test from client without spring boot");
+            System.out.println("result: " + result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+
+/*
         if(System.getSecurityManager() == null){
             System.setSecurityManager(new SecurityManager());
         }
         try {
-            String name = "HelloEclipse";
+            String name = "IHelloWorldFromEclipse";
+            System.out.println(args[0]);
             Registry registry = LocateRegistry.getRegistry(args[0]);
+            
             for (String string : registry.list()) {
                 System.out.println(string);
             }
@@ -19,6 +40,6 @@ public class HelloWorldApp{
             System.out.println("result: " + result);
         } catch (Exception e) {
             e.printStackTrace();
-        }
+        }*/
     }
 }
