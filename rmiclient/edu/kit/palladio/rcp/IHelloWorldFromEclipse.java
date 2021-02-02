e9bce40c1967fca599309d3550b4c20ac99eca18
Malte Reimann <malte.reimann@student.kit.edu>
2021-02-02 08:39:12 +0100

    Cleanup: delete basic RMI Hello World project

--- rmiclient/edu/kit/palladio/rcp/IHelloWorldFromEclipse.java
+++
@@ -1,9 +0,0 @@
-package edu.kit.palladio.rcp;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IHelloWorldFromEclipse extends Remote {
-	String hello(String whatToSay) throws RemoteException;
-
-}
